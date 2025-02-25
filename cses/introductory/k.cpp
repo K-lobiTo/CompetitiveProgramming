d24d3bb5f0c601b15b1dd09831146441957b0c07
K-lobiTo <calebam2112@gmail.com>
2025-02-24 18:23:03 -0600

    introductory problems [k pending]

---
+++ cses/introductory/k.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define ALL(v) (v).begin(), (v).end()
+#define vec vector
+#define snd second
+#define fst first
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+  
+void solve(){
+    ll a,b; cin>>a>>b;
+    if(max(a,b)>(2*min(a,b))){
+        cout<<"NO\n";
+        return;
+    }
+    // while(a && b){
+    //     if(a>b){
+    //         a-=2; b--;
+    //     }
+    //     else {
+    //         a--;b-=2;
+    //     }
+    // }
+    // cout<<((a+b)? "NO\n" : "YES\n" );
+    // cout<<((!(a%2) && !(b%2))? "NO\n" : "YES\n");
+    // cout<<(((a+b)&&!(a%2)&&!(b%2))? "NO\n" : "YES\n");
+    cout<<((!(a+b)||!(a==b && !(a%2)))? "YES\n" : "NO\n");
+    //      are 0s    not both even and equal
+  
+}
+  
+int main(){
+ios::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+cin>>t;
+while(t--){
+solve();
+}return 0;
+}
+  
