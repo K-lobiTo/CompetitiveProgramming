b9f2e069f423286b3861675b8f333c50f5a6cae5
K-lobiTo <calebam2112@gmail.com>
2025-03-01 22:46:11 -0600

    introductory problems until n

--- cses/introductory/n.cpp
+++ cses/introductory/n.cpp
@@ -14,28 +14,19 @@ using namespace std;
 const int MAX = 2e5+20, MOD = 1e9+7;
 int t=1;
   
+void HAN(int n, int a, int b){
+    if(n==1){cout<<a<<' '<<b<<'\n';return;}
+    int B=(6-(a+b));
+    HAN(n-1, a, B);
+    cout<<a<<' '<<b<<'\n';
+    HAN(n-1, B, b);
+
+}
+
 void solve(){
-    int n; cin>>n; 
-    vec<vec<int>> ans(1<<n, vec<int>(n, 0));
-    int jump = 1;
-    int column = (n-1);
-    while (column>=0){
-        for(int i = jump; i<(1<<n); i+=(jump*2)){
-            for(int m = 0; m<(jump*2) && (i+m)<(1<<n);++m){
-                ans[i+m][column]=1;
-            }
-            i+=(jump*2);
-        }
-        column--;
-        jump*=2;
-    }
-    
-    for(auto &row: ans){
-        for(auto &e:row){
-            cout<<e;
-        }
-        cout<<'\n';
-    }
+    int n; cin>>n;
+    cout<<(1<<n)-1<<'\n';
+    HAN(n, 1, 3);
 }
   
 int main(){
