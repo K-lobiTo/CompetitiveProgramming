e9558dffded487d656c97eb4c02e8c05f68e6acf
K-lobiTo <calebam2112@gmail.com>
2025-03-19 23:34:33 -0500

    cses intrudoctory problem q

--- cses/introductory/q.cpp
+++ cses/introductory/q.cpp
@@ -15,15 +15,29 @@ const int MAX = 2e5+20, MOD = 1e9+7;
 int t=1;
 vec<int> iniMat(8, 0);
 
+void priMat(vec<int> mat ){
+    for(int i = 0; i<8; ++i){
+        for(int j = 0; j<8; ++j){
+            if(TEST(iniMat[i], j))cout<<"*";
+            else{
+                cout<<(TEST(mat[i], j)? "Q":"#");
+            }
+        }
+        cout<<'\n';
+    }
+    cout<<"\n\n";
+}
+
 bool test(vec<int> &mat, int i, int j){
-    if(i>7 || j>7)return false;
+    // for(auto &e:mat)cout<<e<<endl;
+    // DEBUG(i);
+    // DEBUG(j);
     if(TEST(iniMat[i], j))return false;
     if (mat[i])return false;
     for(int it = 0; it<8; ++it)if(TEST(mat[it], j))return false;
-    while(i!=0 && j!=0){
-        i--;j--;
-    }
-    for(;i<8 && j<8; ++i, ++j)if(TEST(mat[i], j) || TEST(mat[j], i))return false;
+    int i2 = i, j2 = j;
+    for(;i>=0 && j>=0; --i, --j)if(TEST(mat[i], j))return false;
+    for(;i2>=0 && j2<8; --i2, ++j2)if(TEST(mat[i2], j2))return false;
     return true;
 }
 
@@ -38,21 +52,27 @@ void solve(){
         }
     }
     stack<pair<int, vec<int>>> s; 
-    for(int i = 0; i<8; ++i){
-        vec<int> subM(8);
-        pair<int, vec<int>> e={0, subM};
-        s.push(e);
-    }
+    vec<int> subM(8, 0);
+    s.push({0, subM});
+
     ll ans = 0;
     while (!s.empty()){
-        auto &[idx, subMat] = s.top();
+        pair<int, vec<int>> act = s.top();
         s.pop();
-        if(idx == 8)ans++;
+        int idx = act.first;
+        vec<int> subMat = act.second;
+        // DEBUG(idx);
+        if(idx == 8){
+            ans++;
+            // priMat(subMat);
+        }
         else{
             for(int i = 0; i<8; ++i){
                 if(test(subMat, idx, i)){
+                    // for(auto &e:subMat)cout<<e<<endl;
                     vec<int> nSubMat = subMat;
                     nSubMat[idx] = SET(nSubMat[idx], i);
+                    // pair<int, vec<int>> e={idx+1, nSubMat};
                     s.push({idx+1, nSubMat});
                 }
             }
