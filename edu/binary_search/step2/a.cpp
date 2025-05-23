16a13e6bd85ac7952507a4f6a390e5ffc60b0774
K-lobiTo <calebam2112@gmail.com>
2025-05-22 21:39:34 -0600

    step2 binary search

---
+++ edu/binary_search/step2/a.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+ll w, h, n; 
+
+bool ok(ll v){
+    return ((v/w)*(v/h) >=n);
+}
+  
+void solve(){
+    cin>>w>>h>>n;
+    ll l = 0;
+    ll r = 1;
+    while (!ok(r))r*=2;
+    ll m;
+    while (l+1<r) {
+        m = (l+r)/2LL;
+        if(ok(m))r = m;
+        else l = m;
+    }
+    cout<<r<<endl;
+}
+
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
