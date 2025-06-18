09128c35790de31b5621cfc281365b7e43177b50
K-lobiTo <calebam2112@gmail.com>
2025-06-17 23:16:55 -0600

    binary search step3

---
+++ edu/binary_search/step3/b.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+int n, k;
+vec<ll> a;
+  
+
+bool ok(ll s){
+    int myk = 1;
+    ll sum = 0;
+    for (int i = 0; i < n; i++) {
+        if(a[i] > s)return false;
+        
+        if(sum+a[i] > s){
+            myk++;
+            sum = 0;
+        }
+        sum+=a[i];
+    }
+    return myk<=k;
+}
+
+void solve(){
+    cin>>n>>k;
+    a.resize(n);rAuto(a);
+    ll l = 0;
+    ll r = 1;
+    while(!ok(r))r*=2; // is ok
+    while(l+1 < r){
+        ll mid = (l+r)/2;
+        if(ok(mid))r = mid;
+        else l = mid;
+    }
+    cout<<r<<endl;
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
