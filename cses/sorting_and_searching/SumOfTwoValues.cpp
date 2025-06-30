d39909fbf446ac8ebdd6afdec4cb7dd6442b1691
K-lobiTo <calebam2112@gmail.com>
2025-06-29 23:08:19 -0600

    sorting and searching

---
+++ cses/sorting_and_searching/SumOfTwoValues.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+  
+void solve(){
+    int n, x; cin>>n>>x;
+    vec<int> a(n); rAuto(a);
+    map<int, int> indx;
+    set<int> candidates;
+    for (int i = 0; i < n; i++) {
+        if(candidates.find(a[i]) != candidates.end()){
+            cout<<indx[x-a[i]]<<' '<<i+1<<endl;
+            return;
+        }
+        candidates.insert(x-a[i]);
+        indx[a[i]] = i+1;
+    }
+    cout<<"IMPOSSIBLE"<<endl;
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    // cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
