a8dcab9341b38ba1ddb1d7e49176a7b3c3c2e8f0
K-lobiTo <calebam2112@gmail.com>
2025-08-20 22:42:49 -0600

    step4 binary search

--- edu/binary_search/step4/b.cpp
+++ edu/binary_search/step4/b.cpp
@@ -11,27 +11,93 @@ using namespace std;
 #define rALL(v) (v).rbegin(), (v).rend()
 #define vec vector
 #define ll long long
-const int MAX = 2e5+20, MOD = 1e9+7;
+#define ld long double
+const int MAX = 1e8, MOD = 1e9+7;
 int t=1;
 int n, m; 
-vec<vec<int>> ady;
-  
-bool ok(int m){
-    vec<int> dp(n, 0);
-    // IDGI
-    return true;
+vec<vec<pair<int, int>>> ad;
+vec<int> topoSort;
+
+
+std::vector<int> topologicalSort(int numVertices, const std::vector<std::vector<int>>& adj) { 
+    std::vector<int> inDegree(numVertices, 0);
+    for (int i = 0; i < numVertices; ++i) {
+        for (int neighbor : adj[i]) {
+            inDegree[neighbor]++;
+        }
+    }
+
+    std::queue<int> q;
+    for (int i = 0; i < numVertices; ++i) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    std::vector<int> topoOrder;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        topoOrder.push_back(u);
+
+        for (int v : adj[u]) {
+            inDegree[v]--;
+            if (inDegree[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+    return topoOrder;
+}
+
+bool ok(ld med, vec<int> &path, bool wantPath){
+    vec<pair<int, ld>> dp(n, {-1, MAX});
+    dp[0].second = 0;
+    for(auto &e : topoSort){
+        for(auto &[child, cost] : ad[e]){
+            ld newCost = dp[e].second + cost - med;
+            if(newCost < dp[child].second) dp[child] = {e, newCost};
+        }
+    }
+    if(wantPath){
+        int node = n-1;
+        path.push_back(n-1);
+        while(node>0){
+            path.push_back(dp[node].first);
+            node = dp[node].first;
+        }
+    }
+    return dp[n-1].second <= 0;
 }
 
 void solve(){
     cin>>n>>m; 
-    ady.assign(n, vec<int>(n, 0)); 
-    // junctions are going from lower to higher
+    ad.assign(n, vec<pair<int, int>>()); 
+    vec<vec<int>> adjtp(n);
+    // junctions are going from lower to higher => is DAG
     for (size_t i = 0; i < m; i++) {
         int a, b, c;
         cin>>a>>b>>c; 
-        ady[a-1][b-1]=c;
+        ad[a-1].push_back({b-1, c});
+        adjtp[a-1].push_back(b-1);
     }
-    
+
+    topoSort = topologicalSort(n, adjtp);
+
+    vec<int> ans;
+    ld l = 0;
+    ld r = 2; while(!ok(r, ans, 0))r*=2;
+    const ld ERR = 1e-6;
+    while(l+ERR < r){
+        ld mid = (l+r)/2.0;
+        if(ok(mid, ans, 0))r=mid;
+        else l=mid;
+    }
+
+    ok(r, ans, 1);
+    cout<<ans.size()-1<<endl;
+    for (int i = ans.size()-1; i>=0; i--) cout<<ans[i]+1<<" ";
+    cout<<endl;
     
 
 }
