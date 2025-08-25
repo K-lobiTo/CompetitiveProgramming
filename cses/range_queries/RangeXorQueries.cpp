4d58963305c549283b4eccf57f1254e992fee811
K-lobiTo <calebam2112@gmail.com>
2025-08-24 20:49:27 -0600

    range_queries cses

---
+++ cses/range_queries/RangeXorQueries.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+
+struct Mono {
+    // TODO
+    ll value;
+    Mono(ll v = (1e10)-1LL) : value(v) {}
+};
+
+Mono operator+(Mono a, Mono b) {
+    // TODO
+    return {a.value ^ b.value};
+}
+
+struct Tree {
+  int n;
+  vector<Mono> s;
+
+  Tree(int n) : n(n), s(n<<1) {}
+  Tree(vector<Mono> const& a) : n(a.size()), s(a.size()<<1) {
+    copy(ALL(a), s.begin() + n);
+    for (int i = n - 1; i > 0; --i) s[i] = s[i<<1] + s[i<<1|1];
+  }
+
+  void set(int x, Mono m) {
+    for (s[x += n] = m; x >>= 1;) s[x] = s[x<<1] + s[x<<1|1];
+  }
+
+  Mono get(int l, int r) {
+    Mono resl((1e10)-1LL), resr((1e10)-1LL); // neutral values should be set : resl(0LL), resr(OLL)
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l&1) resl = resl + s[l++];
+      if (r&1) resr = s[--r] + resr;
+    }
+    return resl + resr;
+  }
+};
+  
+void solve(){
+    int n, q; cin>>n>>q;
+    vec<Mono> a(n);
+    for(auto &e:a)cin>>e.value;
+    Tree segTree(a);
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        cin>>l>>r;l--; r--;
+        cout<<segTree.get(l, r+1).value<<endl;
+    }
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
