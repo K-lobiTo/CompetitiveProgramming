38738398b187d58e6138a3428aa7b1316793fa76
K-lobiTo <calebam2112@gmail.com>
2025-08-30 00:15:05 -0600

    two_pointers step3

---
+++ edu/two_pointers/step3/c.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+  
+void solve(){
+    int n, r; cin>>n>>r;
+    vec<ll> d(n); rAuto(d);
+    int rr = 0;
+    ll dist = 0;
+    ll ans = 0;
+    for (int l = 0; l < n; l++) {
+        dist = d[rr]-d[l];
+        while(dist<=r && rr<(n-1)){
+            dist = d[++rr]-d[l];
+        }
+        if(dist<=r)break;
+        ans+=n-rr;
+    }
+    cout<<ans<<endl;
+
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
