18aacdd49e178fba60c01a0af2e29414a28dc6c9
K-lobiTo <calebam2112@gmail.com>
2025-08-29 23:44:55 -0600

    two_pointers step3

---
+++ edu/two_pointers/step3/b.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+  
+void solve(){
+    int n; cin>>n;
+    ll s; cin>>s;
+    vec<ll> a(n); rAuto(a);
+    ll ans = 0;
+    ll csum = 0;
+    int l = 0;
+
+
+    auto gauss = [&](ll v){
+        return (v*(v+1LL))/2;
+    };
+
+    for (int r = 0; r < n; r++) {
+        csum+=a[r];
+        if(csum<=s)ans+=gauss(r-l+1LL);
+        bool f = 0;
+        while(csum>s){
+            csum-=a[l++];
+            f = 1;
+        }
+        if(f)ans+=gauss(r-l+1LL);
+    }
+    cout<<ans<<endl;
+}
+
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
