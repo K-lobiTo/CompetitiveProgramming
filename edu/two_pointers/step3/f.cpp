344699c8742cc3da1436533af3400b0a0f13667d
K-lobiTo <calebam2112@gmail.com>
2025-09-05 12:16:18 -0600

    two_pointers step3

---
+++ edu/two_pointers/step3/f.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+  
+void solve(){
+    int n, m; cin>>n>>m;
+    string s; cin>>s;
+    vec<int> cletters(26);
+    char let;
+    for (int i = 0; i < m; i++) {
+        cin>>let;
+        cletters[let - 'a']++;
+    }
+    int l = 0;
+    vec<int> cur(26);
+    ll ans = 0;
+
+
+    auto notok = [&](vec<int> c) -> bool{
+        for (int i = 0; i < 26; i++) {
+            if(cletters[i]<c[i])return true;
+        }
+        return false;
+    };
+    for (int r = 0; r < s.size(); r++) {
+        cur[s[r]-'a']++;
+        while(notok(cur) && (l< r+1)){
+            cur[s[l]-'a']--;
+            l++;
+        }
+        ans+=r-l+1;
+    }
+    cout<<ans<<endl;
+
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
