010a55c007d957be7689a33f49e66b4e9bbf6295
K-lobiTo <calebam2112@gmail.com>
2025-09-20 17:51:59 -0600

    segment_tree p1 s3

---
+++ edu/segment_tree/step3/e.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+  
+#define SET(m, i) ((m) | (1ULL << (i)))
+#define TEST(m, i) ((m) & (1ULL << (i)))
+#define CLEAR(m, i) ((m) &~ (1ULL << (i)))
+#define DEBUG(n) cout<<#n<<" = "<<n<< endl
+#define MSET(arr, x, n) (memset(arr, x, (n)*sizeof(arr[0])))
+#define rAuto(arr) for(auto &e:arr)cin>>e
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+#define vec vector
+#define ll long long
+const int MAX = 2e5+20, MOD = 1e9+7;
+int t=1;
+
+struct Mono {
+    ll value;
+    Mono(ll v = 0) : value(v) {}
+};
+
+Mono operator+(Mono a, Mono b) {
+    return {a.value + b.value};
+}
+
+struct SegmentTree {
+  int n;
+  vector<Mono> s;
+
+  SegmentTree(int n) : n(n), s(n<<1) {}
+  SegmentTree(vector<Mono> const& a) : n(a.size()), s(a.size()<<1) {
+    copy(ALL(a), s.begin() + n);
+    for (int i = n - 1; i > 0; --i) s[i] = s[i<<1] + s[i<<1|1];
+  }
+
+  void set(int x, Mono m) {
+    for (s[x += n] = m; x >>= 1;) s[x] = s[x<<1] + s[x<<1|1];
+  }
+
+  Mono get(int l, int r) { // [l, r)
+    Mono resl, resr;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l&1) resl = resl + s[l++];
+      if (r&1) resr = s[--r] + resr;
+    }
+    return resl + resr;
+  }
+};
+  
+void solve(){
+    int n, m; cin>>n>>m;
+    SegmentTree sgt(n+1);
+    ll tp, il, r, v;
+    while(m--){
+        cin>>tp>>il;
+        if(tp-1){
+            cout<<sgt.get(0, il+1).value<<endl;
+        }
+        else {
+            cin>>r>>v;
+            sgt.set(il, {sgt.get(il, il+1).value + v});
+            sgt.set(r, {sgt.get(r, r+1).value - v});
+        }
+    }
+}
+  
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
