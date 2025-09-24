17abd24671961c629469cde2f07a62a06dc3f995
K-lobiTo <calebam2112@gmail.com>
2025-09-24 14:08:36 -0600

    contest 1759

--- contests/1759/d.cpp
+++ contests/1759/d.cpp
@@ -14,32 +14,47 @@ using namespace std;
 const int MAX = 2e5+20, MOD = 1e9+7;
 int t=1;
 
-ll myPow( ll e){
-    ll a = 1;
-    while (e--)    {
-        a*=10;
-    }
-    return a;
-}
-  
-int nDigits(int k){
-    return floor(log10(k))+1;
-}
 
 void solve(){
     ll n, m;
     cin>>n>>m;
-    // ll ans = n*m;
-    // if(!(n%2)){
-    //     if(!(n%10))ans = n*m;
-    //     else {
-    //         ans = n*(m/5 * 5);
-    //     }
-    // } 
-    // if(!(n%5)){
-    //     if()
-    // }
+    map<ll, int> fact;
+    ll nf = n;
+    while(n>0 && !(nf%2)){
+        fact[2]++;
+        nf/=2;
+    }
+    while(n>0 && !(nf%5)){
+        fact[5]++;
+        nf/=5;
+    }
+    ll k = 1;
+    while(fact[2] < fact[5]){
+        if(k*2 > m){
+            break;
+        }
+        else{
+            k*=2;
+            fact[2]++;
+        }
+    }
+    while(fact[5] < fact[2]){
+        if(k*5 > m){
+            break;
+        }
+        else{
+            k*=5;
+            fact[5]++;
+        }
 
+    }
+    while(k*10 <= m){
+        k*=10;
+    }
+    if(k==1) return void(cout<<n*m<<endl);
+    k*= m/k;
+    cout<<n*k<<endl;
+    
 }
   
 int main(){
