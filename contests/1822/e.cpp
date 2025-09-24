63bfbd284f2024df8dd3da164b2a449d608ce24b
K-lobiTo <calebam2112@gmail.com>
2025-09-24 16:04:17 -0600

    contest 1822

--- contests/1822/e.cpp
+++ contests/1822/e.cpp
@@ -14,91 +14,53 @@ using namespace std;
 const int MAX = 2e5 + 20, MOD = 1e9 + 7;
 int t = 1;
 
-bool notPalin(string s, int n)
-{
-    for (int i = 0; i < n / 2; ++i)
-    {
-        if (s[i] == s[n - i - 1])
-            return false;
+void solve() {
+    int n, ans = 0; cin >> n;
+    string s; cin >> s;
+    if (n % 2) return void(cout << -1 << endl);
+    vec<int> letters(26);
+    for (int i = 0; i < n; i++) {
+        letters[s[i]-'a']++;
     }
-    return true;
-}
+    for (int i = 0; i < 26; i++) {
+        if(letters[i] > n/2)return void(cout<< -1<<endl);
+    }
 
-void solve()
-{
-    int n, ans = 0;
-    cin >> n;
-    string s;
-    cin >> s;
-    char before;
-    if (n % 2)
-    {
-        cout << -1 << '\n';
-        return;
-    }
-    set<char> letters;
-    for (int i = 0; i < (n / 2); ++i)
-    {
-        if (s[i] == s[n - i - 1])
-        {
-            if (!i)
-            {
-                bool pos = false;
-                for (int j = 0; j < (n / 2) && !pos; ++j)
-                {
-                    if (s[i] != s[j])
-                    {
-                        before = s[i];
-                        s[i] = s[j];
-                        s[j] = before;
-                        ans++;
-                        pos = true;
-                    }
-                }
-                if (!pos)
-                {
-                    cout << -1 << '\n';
-                    return;
-                }
-            }
-            else
-            {
-                bool pos = false;
-                for (int j = 0; j < (n / 2) && !pos; ++j)
-                {
-                    if (s[i] != s[j])
-                    {
-                        before = s[i];
-                        s[i] = s[j];
-                        s[j] = before;
-                        pos = true;
-                        ans++;
-                    }
-                }
-                if (!pos)
-                {
-                    for (int j = i; j >= 0 &&!pos; --j)
-                    {
-                        if (s[j] != s[i])
-                        {
-                            before = s[i];
-                            s[i] = s[j];
-                            s[j] = before;
-                            pos= true;
-                            ans++;
-                        }
-                    }
-                }
-                if (!pos)
-                {
-                    cout << -1 << '\n';
-                    return;
-                }
-            }
+    vec<int> coll(26);
+    for (int i = 0; i < n/2; i++) {
+        if(s[i] == s[n-i-1]){
+            coll[s[i]-'a']++;
         }
     }
-    DEBUG(ans);
-    DEBUG(s);
+    set<pair<int, int>> colli;
+    for (int i = 0; i < 26; i++) {
+        if(coll[i]){
+            colli.insert({coll[i], i});
+        }
+    }
+    while(!colli.empty()){
+        if(colli.size()==1){
+            ans+= (*colli.begin()).first;
+            break;
+        }
+
+        auto last = prev(colli.end());
+        auto llast = prev(last);
+        auto &[r1, c1] = *last;
+        auto &[r2, c2] = *llast;
+
+        ans++;
+        int nr1 = r1 - 1;
+        int nr2 = r2 - 1;
+
+        if(nr1)colli.insert({nr1, c1});
+        if(nr2)colli.insert({nr2, c2});
+
+        colli.erase(last);
+        colli.erase(llast);
+
+    }
+    cout<<ans<<endl;
 }
 
 int main()
